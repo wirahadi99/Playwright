22db642d0db7e061eea2760ed10b9b4f5a1affac
wirahadi <wirahadi99@gmail.com>
2024-10-16 00:54:16 +0700

    checkout in sauce demo test

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,19 +1,28 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  const dataTest = [
-    'standard_user', 'lololo'
-  ]
+test('Checkout in saucedemo', async ({ page }) => {
 
   await page.goto('https://www.saucedemo.com/');
 
-  for (let index = 0; index < dataTest.length; index++) {
     await page.locator('[data-test="username"]').click();
-    await page.locator('[data-test="username"]').fill(dataTest[index]);
+    await page.locator('[data-test="username"]').fill('standard_user');
     await page.locator('[data-test="password"]').click();
     await page.locator('[data-test="password"]').fill('secret_sauce');
     await page.locator('[data-test="login-button"]').click();
-    await page.goto('https://www.saucedemo.com/');
-    await expect(page.locator('[data-test="error"]')).toBeVisible();
-  }
+    expect (page.url()).toBe('https://www.saucedemo.com/inventory.html')
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    expect(page.locator('[data-test="remove-sauce-labs-backpack"]')).toBeVisible();
+    await page.locator('[data-test="shopping-cart-link"]').click();
+
+    expect (page.url()).toBe('https://www.saucedemo.com/cart.html')
+    await page.locator('[data-test="checkout"]').click();
+    await page.locator('[data-test="firstName"]').click();
+    await page.locator('[data-test="firstName"]').fill('mail');
+    await page.locator('[data-test="lastName"]').click();
+    await page.locator('[data-test="lastName"]').fill('lee');
+    await page.locator('[data-test="postalCode"]').click();
+    await page.locator('[data-test="postalCode"]').fill('564556');
+    await page.locator('[data-test="continue"]').click();
+    await page.locator('[data-test="finish"]').click();
+    expect(page.locator('[data-test="complete-header"]')).toContainText('Thank you for your order!');
 });
